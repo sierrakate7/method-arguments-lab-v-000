b2da4fb7121490cc6b5cb1281908c82fbb8f29cd
Sierra McCahon <smitche4@mail.umw.edu>
2018-02-22 16:29:57 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
 
+=======
+>>>>>>> d26e0de8f237bb19a300fbaf11c3c10b5cfc3615
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
